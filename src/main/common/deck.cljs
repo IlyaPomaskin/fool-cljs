1becf861c086e9e5801ed8592bb45caf5aa5f972
Ilya Pomaskin <IlyaPomaskin@users.noreply.github.com>
2023-04-07 16:54:16 +0500

    feat(spec): deck

--- src/main/common/deck.cljs
+++ src/main/common/deck.cljs
@@ -1,7 +1,8 @@
 (ns common.deck
   (:require
    [common.card :as card]
-   [common.consts :refer [ranks suits]]))
+   [common.consts :refer [ranks suits]]
+   [clojure.spec.alpha :as s]))
 
 (defn make []
   (shuffle
@@ -11,11 +12,22 @@
       {:suit suit
        :rank rank}))))
 
+(s/fdef make
+  :ret :deck/deck)
+
 (defn deal-cards [amount deck]
   (split-at amount deck))
 
+(s/fdef deal-cards
+  :args (s/cat :amount number? :deck :deck/deck)
+  :ret (s/tuple :deck/deck :deck/deck))
+
 (defn remove-card [card deck]
   (remove #(card/equals? card %) deck))
+
+(s/fdef remove-card
+  :args (s/cat :card :card/card :deck :deck/deck)
+  :ret :deck/deck)
 
 (defn get-smallest-valuable-card [trump deck]
   (reduce
@@ -23,5 +35,13 @@
    nil
    deck))
 
+(s/fdef get-smallest-valuable-card
+  :args (s/cat :trump :card/suit :deck :deck/deck)
+  :ret :card/card)
+
 (defn mask-deck [deck]
   (map {:hidden true} deck))
+
+(s/fdef mask-deck
+  :args (s/cat :deck :deck/deck)
+  :ret (s/* :deck/deck))
