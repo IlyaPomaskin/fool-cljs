7b048c55de61d33c3c0dfde2cfdfb8a74981db03
Ilya Pomaskin <IlyaPomaskin@users.noreply.github.com>
2023-06-25 17:44:04 +0500

    feat(ui): extract player-ui

--- src/main/frontend/main.cljs
+++ src/main/frontend/main.cljs
@@ -1,80 +1,17 @@
 (ns frontend.main
   (:require
-   [clojure.pprint :refer [pprint]]
    [common.card :as c]
    [common.player :as p]
    [frontend.cardui :as card-ui]
    [frontend.game-context :as game-context]
+   [frontend.playerui :as player-ui]
    [frontend.stackui :as stack-ui]
    [frontend.ui :as ui]
    [uix.core :refer [$ defui]]
    [uix.dom]))
 
-(def player-state-context (uix.core/create-context []))
-(defui player-state-provider [{:keys [children]}]
-  (let [[player-state set-player-state!]
-        (uix.core/use-state
-         {:table-selected nil
-          :player-selected nil})]
-    (pprint player-state)
-    ($ (.-Provider player-state-context) {:value [player-state set-player-state!]}
-       children)))
-
-(defui player-controls [{:keys [class player]}]
-  (let [[_ dispatch!] (uix.core/use-context game-context/context)
-        [player-state] (uix.core/use-context player-state-context)]
-    ($ :.flex.gap-1 {:class class}
-       ($ ui/button {:size :xs
-                     :on-click #(dispatch! {:type :move
-                                            :player player
-                                            :card (:player-selected player-state)})}
-          "Move")
-       ($ ui/button {:size :xs
-                     :on-click #(dispatch! {:type :pass
-                                            :player player})}
-          "Pass")
-       ($ ui/button {:size :xs
-                     :on-click #(dispatch! {:type :beat
-                                            :player player
-                                            :to (:table-selected player-state)
-                                            :by (:player-selected player-state)})}
-          "Beat")
-       ($ ui/button {:size :xs
-                     :on-click #(dispatch! {:type :take
-                                            :player player})}
-          "Take"))))
-
-(defui player [{:keys [player attacker? defender?]}]
-  (let [[player-state set-player-state!] (uix.core/use-context player-state-context)]
-    ($ ui/panel
-       ($ ui/title (:id player))
-
-       (when attacker?
-         ($ :div "attack"))
-
-       (when defender?
-         ($ :div "defend"))
-
-       ($ :.flex.flex-row.flex-wrap.gap-2
-          (map
-           (fn [card]
-             ($ card-ui/visible
-                {:key (card-ui/to-string card)
-                 :card card
-                 :on-click #(set-player-state!
-                             (update
-                              player-state
-                              :player-selected
-                              (fn [prev-card] (if (c/equals? prev-card card) nil card))))
-                 :selected (c/equals? card (:player-selected player-state))}))
-           (:cards player)))
-
-       ($ player-controls
-          {:class "pt-4"
-           :player player}))))
-
 (defui table [{:keys [class table]}]
-  (let [[player-state set-player-state!] (uix.core/use-context player-state-context)]
+  (let [[player-state set-player-state!] (uix.core/use-context player-ui/player-state-context)]
 
     ($ ui/panel
        {:class [class]}
@@ -113,7 +50,7 @@
 
      (map
       (fn [item]
-        ($ player
+        ($ player-ui/player
            {:key (:id item)
             :player item
             :attacker? (p/equals? (:attacker game) item)
@@ -133,7 +70,7 @@
   (uix.dom/render-root
    ($ game-context/provider
       {:player-ids ["qwe" "asd" "zxc"]}
-      ($ player-state-provider
+      ($ player-ui/player-state-provider
          ($ app)))
    root))
 
---
+++ src/main/frontend/playerui.cljs
@@ -0,0 +1,72 @@
+(ns frontend.playerui
+  (:require
+   [clojure.pprint :refer [pprint]]
+   [common.card :as c]
+   [frontend.cardui :as card-ui]
+   [frontend.game-context :as game-context]
+   [frontend.ui :as ui]
+   [uix.core :refer [$ defui]]
+   [uix.dom]))
+
+(def player-state-context (uix.core/create-context []))
+(defui player-state-provider [{:keys [children]}]
+  (let [[player-state set-player-state!]
+        (uix.core/use-state
+         {:table-selected nil
+          :player-selected nil})]
+    (pprint player-state)
+    ($ (.-Provider player-state-context) {:value [player-state set-player-state!]}
+       children)))
+
+(defui player-controls [{:keys [class player]}]
+  (let [[_ dispatch!] (uix.core/use-context game-context/context)
+        [player-state] (uix.core/use-context player-state-context)]
+    ($ :.flex.gap-1 {:class class}
+       ($ ui/button {:size :xs
+                     :on-click #(dispatch! {:type :move
+                                            :player player
+                                            :card (:player-selected player-state)})}
+          "Move")
+       ($ ui/button {:size :xs
+                     :on-click #(dispatch! {:type :pass
+                                            :player player})}
+          "Pass")
+       ($ ui/button {:size :xs
+                     :on-click #(dispatch! {:type :beat
+                                            :player player
+                                            :to (:table-selected player-state)
+                                            :by (:player-selected player-state)})}
+          "Beat")
+       ($ ui/button {:size :xs
+                     :on-click #(dispatch! {:type :take
+                                            :player player})}
+          "Take"))))
+
+(defui player [{:keys [player attacker? defender?]}]
+  (let [[player-state set-player-state!] (uix.core/use-context player-state-context)]
+    ($ ui/panel
+       ($ ui/title (:id player))
+
+       (when attacker?
+         ($ :div "attack"))
+
+       (when defender?
+         ($ :div "defend"))
+
+       ($ :.flex.flex-row.flex-wrap.gap-2
+          (map
+           (fn [card]
+             ($ card-ui/visible
+                {:key (card-ui/to-string card)
+                 :card card
+                 :on-click #(set-player-state!
+                             (update
+                              player-state
+                              :player-selected
+                              (fn [prev-card] (if (c/equals? prev-card card) nil card))))
+                 :selected (c/equals? card (:player-selected player-state))}))
+           (:cards player)))
+
+       ($ player-controls
+          {:class "pt-4"
+           :player player}))))
